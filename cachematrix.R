37a088b953334c81abc9dce296912fbc956c0a8d
ruben.nunez <ruben.nunez@campofriofg.com>
2019-11-01 02:17:07 +0100

    programing assigment 2.1

--- cachematrix.R
+++ cachematrix.R
@@ -5,14 +5,24 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
-        m <- NULL
-        set <- function(y) {
+        m <- NULL ##Initialize the local matrix parameter
+        
+        
+        ##Matrix management
+        
+        set <- function(y) {    ##set the matix
                 x <<- y
                 m <<- NULL
         }
-        get <- function() x
-        setrev <- function(solve) m <<- solve
-        getrev <- function() m
+        
+        get <- function() x     ##get the matrix
+        
+        
+        ##Reverse management
+        
+        setrev <- function(solve) m <<- solve   ##apply the reverse of the matrix
+        
+        getrev <- function() m                  ##retrieve the reverse value of the matrix
         
         list(set = set, get = get,
              setrev = setrev,
@@ -27,11 +37,17 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         
-        m <- x$getrev()
-        if(!is.null(m)) {
+        m <- x$getrev()      ##The reverse value is assigned
+        
+        if(!is.null(m)) {    ##Check caching
+                
                 message("getting cached data")
                 return(m)
+                
         }
+        
+        ##Not chached calculation applied
+        
         data <- x$get()
         m <- mean(data, ...)
         x$setrev(m)
